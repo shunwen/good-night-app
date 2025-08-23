f258beeb05e542fda3a13f01170b0a79de5987b8
shunwen <wenh@mm.st>
2025-08-23 21:25:05 +0800

    feat: add FollowingsController

---
+++ app/controllers/users/followings_controller.rb
@@ -0,0 +1,31 @@
+class Users::FollowingsController < ApplicationController
+  def create
+    @user = User.find(params[:followed_id])
+    @follow = Current.user.followings.build(followed: @user)
+
+    respond_to do |format|
+      if @follow.save
+        format.html { redirect_to @user, notice: "You are now following #{@user.name}." }
+        format.json { render json: @follow, status: :created }
+      else
+        format.html { redirect_to @user, alert: "Unable to follow #{@user.name}." }
+        format.json { render json: @follow.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:followed_id])
+    @follow = Current.user.followings.find_by(followed: @user)
+
+    respond_to do |format|
+      if @follow&.destroy
+        format.html { redirect_to @user, notice: "You unfollowed #{@user.name}." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to @user, alert: "Unable to unfollow #{@user.name}." }
+        format.json { render json: { error: "Unable to unfollow #{@user.name}." }, status: :unprocessable_entity }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :follows
+
+  namespace :users do
+    post :followings, to: "followings#create"
+    delete :followings, to: "followings#destroy"
+  end
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
