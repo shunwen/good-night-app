35b39a982d969850dbc57a97809cc1953e18f331
shunwen <wenh@mm.st>
2025-08-24 19:07:01 +0800

    feat: remove allow_unauthenticated_access from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  allow_unauthenticated_access only: %i[new create]
-
   before_action :set_user, only: %i[show destroy]
 
   # GET /users or /users.json
