eadd826adb371a5734639fb65ce2443fb38d23c6
shunwen <wenh@mm.st>
2025-08-24 18:12:44 +0800

    fix: UsersControllerTest with redirects for unauthenticated access

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,11 +3,18 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @other_user = users(:two)
   end
 
-  test "should get index" do
+  test "should get index when authenticated" do
+    cookies[:user_id] = @user.id
     get users_url
     assert_response :success
+  end
+
+  test "should redirect index when not authenticated" do
+    get users_url
+    assert_response :redirect
   end
 
   test "should get new" do
@@ -23,26 +30,53 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to user_url(User.last)
   end
 
-  test "should show user" do
-    get user_url(@user)
+  test "should show user when authenticated" do
+    cookies[:user_id] = @user.id
+    get user_url(@other_user)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should redirect show when not authenticated" do
+    get user_url(@user)
+    assert_response :redirect
+  end
+
+  test "should get edit when authenticated" do
+    cookies[:user_id] = @user.id
     get edit_user_url(@user)
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name } }
+  test "should redirect edit when not authenticated" do
+    get edit_user_url(@user)
+    assert_response :redirect
+  end
+
+  test "should update user when authenticated" do
+    cookies[:user_id] = @user.id
+    patch user_url(@user), params: { user: { name: "Updated Name" } }
     assert_redirected_to user_url(@user)
   end
 
-  test "should destroy user" do
+  test "should redirect update when not authenticated" do
+    patch user_url(@user), params: { user: { name: "Updated Name" } }
+    assert_response :redirect
+  end
+
+  test "should destroy user when authenticated" do
+    cookies[:user_id] = @user.id
     assert_difference("User.count", -1) do
       delete user_url(@user)
     end
 
     assert_redirected_to users_url
+  end
+
+  test "should redirect destroy when not authenticated" do
+    assert_no_difference("User.count") do
+      delete user_url(@user)
+    end
+    
+    assert_response :redirect
   end
 end
